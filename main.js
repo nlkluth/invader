a9858fa4c50099cb64ebb20bbca1545aa2aaa392
Nathan Kluth <nlkluth@gmail.com>
2014-07-17 10:47:16 -0600

    setting up the player

--- main.js
+++ main.js
@@ -3,14 +3,16 @@ var player, game = new Phaser.Game(400, 300, Phaser.AUTO, 'gameDiv');
 
 var mainState = {
   preload: function() {
-    game.stage.backgroundColor = '#3498db';
+    game.load.image('player', 'assets/player.png');
   },
   create: function() {
+    game.stage.backgroundColor = '#3498db';
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    player = game.add.sprite(game.world.centerX, game.world.centerY, 'player');
   },
   update: function() {
   }
 };
 
-var game = new Phaser.Game(400, 300, Phaser.AUTO, 'gameDiv');
 game.state.add('main', mainState);
 game.state.start('main');
