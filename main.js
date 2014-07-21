a44cd1a266b4b20547340ba5fc664a11e398287a
Nathan Kluth <nlkluth@gmail.com>
2014-07-21 08:12:39 -0600

    adding walls

--- main.js
+++ main.js
@@ -5,16 +5,26 @@ var player, cursor,
 var mainState = {
   preload: function() {
     game.load.image('player', 'assets/player.png');
+    game.load.image('wallV', 'assets/wallVertical.png');
+    game.load.image('wallH', 'assets/wallHorizontal.png');
   },
   create: function() {
     game.stage.backgroundColor = '#3498db';
     game.physics.startSystem(Phaser.Physics.ARCADE);
+
     player = game.add.sprite(game.world.centerX, game.world.centerY, 'player');
     player.anchor.setTo(0.5, 0.5);
     game.physics.arcade.enable(player);
     player.body.gravity.y = 500;
 
     cursor = game.input.keyboard.createCursorKeys();
+    var leftWall = game.add.sprite(0, 0, 'wallV');
+    game.physics.arcade.enable(leftWall);
+    leftWall.body.immovable = true;
+
+    var rightWall = game.add.sprite(480, 0, 'wallV');
+    game.physics.arcade.enable(rightWall);
+    rightWall.body.immovable = true;
   },
   update: function() {
     this.movePlayer();
