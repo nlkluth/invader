b9a48e68d36932429911fc27048328d9904711f6
Nathan Kluth <nlkluth@gmail.com>
2014-07-21 08:16:57 -0600

    build all the walls

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ var mainState = {
     game.load.image('wallV', 'assets/wallVertical.png');
     game.load.image('wallH', 'assets/wallHorizontal.png');
   },
+
   create: function() {
     game.stage.backgroundColor = '#3498db';
     game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -19,14 +20,13 @@ var mainState = {
 
     cursor = game.input.keyboard.createCursorKeys();
 
-    var walls = game.add.group();
-    walls.enableBody = true;
-    game.add.sprite(0, 0, 'wallV', 0, walls);
-    walls.setAll('body.immovable', true);
+    this.createWorld();
   },
+
   update: function() {
     this.movePlayer();
   },
+
   movePlayer: function() {
     if (cursor.left.isDown) {
       player.body.velocity.x = -200;
@@ -39,6 +39,25 @@ var mainState = {
     if (cursor.up.isDown && player.body.touching.down) {
       player.body.velocity.y = -320;
     }
+  },
+  createWorld: function() {
+    var walls = game.add.group();
+    walls.enableBody = true;
+    game.add.sprite(0, 0, 'wallV', 0, walls); // Left
+    game.add.sprite(480, 0, 'wallV', 0, walls); // Right
+    game.add.sprite(0, 0, 'wallH', 0, walls); // Top left
+    game.add.sprite(300, 0, 'wallH', 0, walls); // Top right
+    game.add.sprite(0, 320, 'wallH', 0, walls); // Bottom left
+    game.add.sprite(300, 320, 'wallH', 0, walls); // Bottom right
+    game.add.sprite(-100, 160, 'wallH', 0, walls); // Middle left
+    game.add.sprite(400, 160, 'wallH', 0, walls); // Middle right
+
+    var middleTop = game.add.sprite(100, 80, 'wallH', 0, walls);
+    middleTop.scale.setTo(1.5, 1);
+    var middleBottom = game.add.sprite(100, 240, 'wallH', 0, walls);
+    middleBottom.scale.setTo(1.5, 1);
+
+    walls.setAll('body.immovable', true);
   }
 };
 
